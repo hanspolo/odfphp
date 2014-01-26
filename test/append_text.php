812cbf5abdb3c1072e3c124faddad92e751495a1
hanspolo <ph.hanspolo@googlemail.com>
2014-01-26 16:41:55 +0100

    Created Test for images and frames

--- test/append_text.php
+++ test/append_text.php
@@ -8,6 +8,11 @@ $text->open("./append_text_input.odt");
 
 $content = ODF_Text::getContentBody($text);
 
+$path = $text->addPicture("./test_image1.jpg");
+$image = ODF_Draw::createImage($text->content, $path);
+$frame = ODF_Draw::createFrame($text->content, $image);
+$p = ODF_Text::createParagraph($text->content, $frame);
 
+$content->appendChild($p);
 
 $text->save("append_text_output.odt");
