e11494a85902474f9e9b815479b61126e85676a6
hanspolo <ph.hanspolo@googlemail.com>
2014-02-03 21:39:25 +0100

    Extended create_spreadsheet Test

--- test/create_spreadsheet.php
+++ test/create_spreadsheet.php
@@ -6,11 +6,15 @@ require_once __DIR__ . "/../src/odf_shortcut.php";
 $sheet = new ODF();
 $sheet->create("spreadsheet");
 
-
 $content = ODF_Spreadsheet::getContentBody($sheet);
 
+$cell1 = ODF_Spreadsheet::createTableCell($sheet->content, "Cell 1");
+$cell2 = ODF_Spreadsheet::createTableCell($sheet->content, "Cell 2");
 
-//$content->appendChild(ODF_Spreadsheet::createRow($sheet->content), null);
-		      
+$row = ODF_Spreadsheet::createTableRow($sheet->content, null);
+$row->appendChild($cell1);
+$row->appendChild($cell2);
+
+$content->appendChild(ODF_Spreadsheet::createTable($sheet->content, $row));
 
 $sheet->save("create_spreadsheet_output.ods");
